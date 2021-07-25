702271d7d94fbcf65927e46841dd87cf2bdbc768
nipun.jindal <mansapatiala@gmail.com>
2021-07-25 14:02:23 +0530

    corrects name and Get method

--- repository/ingredient.go
+++ repository/ingredient.go
@@ -2,8 +2,8 @@ package repository
 
 import "nipun.io/brew_machine/domain"
 
-type Ingredient interface {
+type IngredientRepository interface {
 	AddNew(ingredient domain.Ingredient) error
 	UpdateQuantity(name string, delta int) error
-	Get(name string) (domain.Ingredient, error)
+	Get(name string) (*domain.Ingredient, error)
 }
